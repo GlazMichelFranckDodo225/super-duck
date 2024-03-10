7857abf834699696732d5656752cf288143c1be7
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-10 02:51:37 +0100

    Duck Subtypes Mallard and Mandarin

---
+++ src/com/dgmf/Mallard.java
@@ -0,0 +1,11 @@
+package com.dgmf;
+
+// Each Duck Subtype is Responsible for Implementing the
+// Behavior of the "display()" Method for How It Will
+// Appear on the Screen
+public class Mallard extends Duck {
+    @Override
+    public void display() {
+        // Appearance of a Mallard
+    }
+}
